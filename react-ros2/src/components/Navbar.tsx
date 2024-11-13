c6fbd19f74889fc4c7a17f63315988221551bb4a
Blacklotus88888 <Song.yang.chen89898@gmail.com>
2024-11-13 00:20:33 -0500

    fixing typos, adding styles, planning and improving node controller component

--- react-ros2/src/components/Navbar.tsx
+++ react-ros2/src/components/Navbar.tsx
@@ -6,7 +6,6 @@ function Navbar() {
         <div>
             <nav style={{display: "flex", justifyContent: "space-around"}}>
                 <Link to="/home">Home</Link>
-                |
                 <Link to="/sandbox">Sandbox</Link>
             </nav>
         </div>
